6a689238d28b6abce13e83aeb9a7d3d457a22a23
Ryotaro Sakai <sakai@cloudbuilders.jp>
2025-09-30 15:36:43 +0000

    パイプラインスタックにGitHub接続を追加し、CodePipelineの接続ARNを動的に取得するように変更。ビルド仕様のデフォルト設定を更新。

--- infra/cdk/stacks/PipelineStack.ts
+++ infra/cdk/stacks/PipelineStack.ts
@@ -1,5 +1,6 @@
 import { Stack, StackProps, Stage, StageProps } from 'aws-cdk-lib';
 import { BuildSpec } from 'aws-cdk-lib/aws-codebuild';
+import { CfnConnection } from 'aws-cdk-lib/aws-codeconnections';
 import { PipelineType } from 'aws-cdk-lib/aws-codepipeline';
 import * as pipelines from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
@@ -17,10 +18,10 @@ export class PipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const connectionArn = process.env.CONNECTION_ARN;
-    if (!connectionArn) {
-      throw new Error('Environment variable CONNECTION_ARN is required for CodeStar Connections');
-    }
+    const gitHubConnection = new CfnConnection(this, 'GitHubConnection', {
+      connectionName: `GitHubConnection-${this.stackName}`,
+      providerType: 'GitHub',
+    });
 
     const pipeline = new pipelines.CodePipeline(this, 'Pipeline', {
       pipelineName: 'SimpleTodoPipeline',
@@ -28,10 +29,11 @@ export class PipelineStack extends Stack {
       selfMutation: true,
       synth: new pipelines.ShellStep('Synth', {
         input: pipelines.CodePipelineSource.connection('s4k4x4r0/simple-todo-list', 'main', {
-          connectionArn,
+          connectionArn: gitHubConnection.ref,
         }),
-        installCommands: ['npm ci', 'npx playwright install --with-deps'],
         commands: [
+          'npm ci',
+          'npx playwright install --with-deps',
           'npm run format:check',
           'npm run lint',
           'npm run typecheck',
@@ -40,7 +42,7 @@ export class PipelineStack extends Stack {
           'npx cdk synth',
         ],
       }),
-      synthCodeBuildDefaults: {
+      codeBuildDefaults: {
         partialBuildSpec: BuildSpec.fromObject({
           phases: {
             install: {
